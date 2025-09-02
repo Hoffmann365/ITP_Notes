4186e06813d31e9bcacc83e0df5ffedcaa1c8f11
Davi Hoffmann <daviwhagner@gmail.com>
2025-09-02 10:23:09 -0300

    solution 1028.c - lista3

--- beecrowd/lista3-repeticao/1028.c
+++ beecrowd/lista3-repeticao/1028.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a, b, count = 0;
+
+    scanf("%d", &count);
+
+    for (int i = 0; i < count; i++)
+    {
+        scanf("%d %d", &a, &b);
+        while (b != 0)
+        {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        printf("%d\n",a);
+    }
 
     return 0;
 }
